c195dd765973001a4cffc40ec94cce7b30b0d591
Mercy1998 <1143776778@qq.com>
2019-01-18 18:52:02 +0800

    Door

---
+++ assets/script/Door.js
@@ -0,0 +1,30 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad:function () {},
+
+    //从出口进入
+    showWarning:function() {
+        AlertWarn.show("警告","出口无法进入！")
+    },
+
+    //刷脸进入
+    showFaceEnter:function() {
+        AlertWarn.show("恭喜","人脸认证成功！")
+    },
+
+    //刷卡进入
+    showDoorEnter:function() {
+        AlertWarn.show("嘀嘀","刷卡成功！")
+    }
+
+   
+
+    // update (dt) {},
+});
